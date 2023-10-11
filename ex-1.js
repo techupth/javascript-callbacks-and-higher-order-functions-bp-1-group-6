2671a2cc01d4071958f71c95238cb03336a54200
Kantima <k.chooarwut@gmail.com>
2023-10-11 16:27:08 +0700

    feat: assignment Callbacks and Higher Order Functions from earth (Ex.1-2)

--- ex-1.js
+++ ex-1.js
@@ -1,9 +1,21 @@
 // Exercise #1: For Each Function
 
-function forEach(array, operation) {
+function forEach(array,operation) {
   // Start coding here
+  const salary = [];
+  for (let i=0; i < array.length; i++){
+    if (operation(array[i])){
+      salary.push(operation(array[i]));
+    };
+  }
+  return salary
 }
+
+function newSalary(old) {
+  return old + 5000;
+}
+
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+const newEmployeeSalaries = forEach(employeeSalaries,newSalary);
 // Using `forEach` function here
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,7 +2,21 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for (let i=0; i < array.length; i++){
+    if(array[i] >= 70){
+      count = count + 1;
+     }
+    //console.log(array[i] + " in Rount "+ (i+1) + " : count = " + count); //show ค่าของ array และ การนับ count
+  }
+//console.log("\nSummary Count = "+ count+"\n") //จำนวนนักเรียนที่มีคะแนนมากกว่าหรือเท่ากับ 5
+return operation(count);
 }
+
+const count = (num) => {
+  return num >= 5? true:false;
+}
+
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
@@ -10,6 +24,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,count);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,count);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,count);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
