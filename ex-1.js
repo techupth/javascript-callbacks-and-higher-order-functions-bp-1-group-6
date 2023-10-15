08c4cfa7d21736bd053884df54e29650d520fa51
Natkamon Saelim <pim-i-smart@hotmail.com>
2023-10-15 11:33:50 +0700

    feat: assignment callbacks from pim

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,22 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let i = 0; i < array.length; i++) {
+    const currentSalary = array[i];
+    const newSalary = operation(currentSalary);
+    newEmployeeSalaries.push(newSalary);
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+// สร้าง Callback Function ที่จะเพิ่มเงินเดือนไป 5000 บาท
+function increaseSalary(salary) {
+  return salary + 5000;
+}
+// ใช้ forEach เพื่อปรับเงินเดือนของพนักงานและบันทึกเงินเดือนใหม่ลงใน newEmployeeSalaries
+forEach(employeeSalaries, increaseSalary);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,8 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  const count = array.filter(operation).length;
+  return count >= 5;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -9,7 +11,20 @@ const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
+// สร้าง Callback Function ที่ตรวจสอบคะแนนมากกว่า 70
+function checkAbove70(score) {
+  return score > 70;
+}
 
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+
+// ใช้ atLeastFive เพื่อตรวจสอบคะแนนของนักเรียนแต่ละห้อง
+scoreRoom1Result = atLeastFive(studentScoresRoom1, checkAbove70);
+scoreRoom2Result = atLeastFive(studentScoresRoom2, checkAbove70);
+scoreRoom3Result = atLeastFive(studentScoresRoom3, checkAbove70);
+
+console.log("Room 1:", scoreRoom1Result);
+console.log("Room 2:", scoreRoom2Result);
+console.log("Room 3:", scoreRoom3Result);
